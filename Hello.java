9df6657d96eb8df257ccce531b452d23552b9b61
Ramesh Reddy <rameshbuddareddy@gmail.com>
2018-08-17 12:05:10 +0530

    modified Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ class Hello{
 	public static void main(String args[]){
 		
 		for(int i=1; i<=10; i++){
-			System.out.println("Hello World "+i);
+			System.out.println("Hello World "+i)
 		}
 			
 	}
